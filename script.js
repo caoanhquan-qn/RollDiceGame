ac3a870e6a979e71c9f87c17b6d07ca1aea1a4b0
=caoanhquan <caoanhquan_qn@yahoo.com>
2020-12-23 14:42:30 +1100

    clean code

--- script.js
+++ script.js
@@ -20,73 +20,33 @@ let score1 = 0;
 let score2 = 0;
 let currentScore1 = 0;
 let currentScore2 = 0;
+function togglePlayerActive() {
+  player1.classList.toggle("player--active");
+  player2.classList.toggle("player--active");
+}
+function disabledButton() {
+  buttonRollDice.disabled = true;
+  buttonHold.disabled = true;
+}
 buttonRollDice.addEventListener("click", function () {
   const rollDiceNumber = Math.floor(Math.random() * 6) + 1;
+  dice.style.display = "block";
+  dice.src = `dice-${rollDiceNumber}.png`;
   if (player1.classList.contains("player--active")) {
     if (rollDiceNumber === 1) {
-      dice.style.display = "block";
-      dice.src = "dice-1.png";
       currentScore1 = 0;
       player1CurrentScore.innerText = 0;
-      player1.classList.toggle("player--active");
-      player2.classList.toggle("player--active");
-    } else if (rollDiceNumber === 2) {
-      dice.style.display = "block";
-      dice.src = "dice-2.png";
-      currentScore1 += rollDiceNumber;
-      player1CurrentScore.innerText = currentScore1;
-    } else if (rollDiceNumber === 3) {
-      dice.style.display = "block";
-      dice.src = "dice-3.png";
-      currentScore1 += rollDiceNumber;
-      player1CurrentScore.innerText = currentScore1;
-    } else if (rollDiceNumber === 4) {
-      dice.style.display = "block";
-      dice.src = "dice-4.png";
-      currentScore1 += rollDiceNumber;
-      player1CurrentScore.innerText = currentScore1;
-    } else if (rollDiceNumber === 5) {
-      dice.style.display = "block";
-      dice.src = "dice-5.png";
-      currentScore1 += rollDiceNumber;
-      player1CurrentScore.innerText = currentScore1;
-    } else if (rollDiceNumber === 6) {
-      dice.style.display = "block";
-      dice.src = "dice-6.png";
+      togglePlayerActive();
+    } else {
       currentScore1 += rollDiceNumber;
       player1CurrentScore.innerText = currentScore1;
     }
   } else if (player2.classList.contains("player--active")) {
     if (rollDiceNumber === 1) {
-      dice.style.display = "block";
-      dice.src = "dice-1.png";
       currentScore2 = 0;
       player2CurrentScore.innerText = 0;
-      player1.classList.toggle("player--active");
-      player2.classList.toggle("player--active");
-    } else if (rollDiceNumber === 2) {
-      dice.style.display = "block";
-      dice.src = "dice-2.png";
-      currentScore2 += rollDiceNumber;
-      player2CurrentScore.innerText = currentScore2;
-    } else if (rollDiceNumber === 3) {
-      dice.style.display = "block";
-      dice.src = "dice-3.png";
-      currentScore2 += rollDiceNumber;
-      player2CurrentScore.innerText = currentScore2;
-    } else if (rollDiceNumber === 4) {
-      dice.style.display = "block";
-      dice.src = "dice-4.png";
-      currentScore2 += rollDiceNumber;
-      player2CurrentScore.innerText = currentScore2;
-    } else if (rollDiceNumber === 5) {
-      dice.style.display = "block";
-      dice.src = "dice-5.png";
-      currentScore2 += rollDiceNumber;
-      player2CurrentScore.innerText = currentScore2;
-    } else if (rollDiceNumber === 6) {
-      dice.style.display = "block";
-      dice.src = "dice-6.png";
+      togglePlayerActive();
+    } else {
       currentScore2 += rollDiceNumber;
       player2CurrentScore.innerText = currentScore2;
     }
@@ -100,32 +60,20 @@ buttonHold.addEventListener("click", function () {
     player1CurrentScore.innerText = currentScore1;
     if (score1 >= 100) {
       player1.classList.add("player--winner");
-      buttonRollDice.disabled = true;
-      buttonHold.disabled = true;
-    } else if (score2 >= 100) {
-      player2.classList.add("player--winner");
-      buttonRollDice.disabled = true;
-      buttonHold.disabled = true;
+      disabledButton();
     } else {
-      player1.classList.toggle("player--active");
-      player2.classList.toggle("player--active");
+      togglePlayerActive();
     }
   } else if (player2.classList.contains("player--active")) {
     score2 += currentScore2;
     player2Score.innerText = score2;
     currentScore2 = 0;
     player2CurrentScore.innerText = currentScore2;
-    if (score1 >= 100) {
-      player1.classList.add("player--winner");
-      buttonRollDice.disabled = true;
-      buttonHold.disabled = true;
-    } else if (score2 >= 100) {
+    if (score2 >= 100) {
       player2.classList.add("player--winner");
-      buttonRollDice.disabled = true;
-      buttonHold.disabled = true;
+      disabledButton();
     } else {
-      player1.classList.toggle("player--active");
-      player2.classList.toggle("player--active");
+      togglePlayerActive();
     }
   }
 });
